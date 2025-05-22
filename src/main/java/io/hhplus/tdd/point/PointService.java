cdde6b3ff4e8e6af002031268445eb1bb8685026
Dior <chinup1004@naver.com>
2025-05-23 05:54:10 +0900

    JVM 수준의 스레드 락(synchronized) 활용해서 동시 요청 순서 보장

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
 @Service
@@ -13,14 +15,23 @@ public class PointService {
 
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
+
     private static final long MAX_POINT = 1000000L;
 
-    public UserPoint charge(long id, long amount) {
-        UserPoint currentUserPoint = userPointTable.selectById(id);
-        UserPoint updatedUserPoint = currentUserPoint.charge(amount);
+    private final Map<Long, Object> locks = new ConcurrentHashMap<>();
 
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
-        return userPointTable.insertOrUpdate(id, updatedUserPoint.point());
+    private Object getUserLock(long userId) {
+        return locks.computeIfAbsent(userId, id -> new Object());
+    }
+
+    public UserPoint charge(long id, long amount) {
+        synchronized (getUserLock(id)) {
+            UserPoint currentUserPoint = userPointTable.selectById(id);
+            UserPoint updatedUserPoint = currentUserPoint.charge(amount);
+
+            pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+            return userPointTable.insertOrUpdate(id, updatedUserPoint.point());
+        }
     }
 
     public UserPoint useUserPoint(long id, long amount) {
