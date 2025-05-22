11649c83d34d08d08b570b18ce568064b8d00691
Dior <chinup1004@naver.com>
2025-05-23 01:51:31 +0900

    TDD GREEN: 포인트 충전 최대 잔고 정책 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -11,10 +11,15 @@ public class PointService {
 
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
+    private static final long MAX_POINT = 1000000L;
 
     public UserPoint charge(long id, long amount) {
         UserPoint currentUserPoint = userPointTable.selectById(id);
         long updatedAmount = currentUserPoint.point() + amount;
+
+        if (updatedAmount > MAX_POINT) {
+            throw new IllegalStateException("최대 포인트 잔고를 초과했습니다.");
+        }
 
         return userPointTable.insertOrUpdate(id, updatedAmount);
     }
